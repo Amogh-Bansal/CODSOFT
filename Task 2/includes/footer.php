fe967e2e4652285cbb9f1f597ee8a166832f883b
Amogh-Bansal <143338433+Amogh-Bansal@users.noreply.github.com>
2023-08-29 19:17:01 +0530

    Add files via upload

---
+++ Task 2/includes/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+    <h2 style="text-align: center; padding-top: 30px;">&copy Amogh Bansal 21803008</h2>
+</div>
---
+++ Task 2/includes/payment.php
@@ -0,0 +1,4 @@
+<div>
+    <h2 align="center">Pay now with Paypal:</h2>
+    <p style="text-align: center;"><img src="paytm.jpg" width="300" height="150"></p>
+</div>
